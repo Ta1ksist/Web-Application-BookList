512aeff18a6cb975c12bfff876e65d7653f44068
Izzat_Melikov <gagarin.yurick@mail.ru>
2025-01-18 00:46:54 +0300

    UpdateBook page

---
+++ App/Frontend/booklist/app/UpdateBook/page.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import GetBooks from '@/lib/api/FetchBook';
+import { Link } from "react-router-dom";
+import '@/styles/pages/UpdateBook.css';
+
+
+export default function UpdateBook(){
+    const [books, setBooks] = useState<Book[]>([]);
+        useEffect(() => {
+            GetBooks().then(setBooks).catch(console.error);
+        },[]);
+    return(
+        <main>
+            <h1 className="main-h1">Update Book</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>
+                        Title
+                    </th>
+                    <th>
+                        Description
+                    </th>
+                    <th>
+                        Author
+                    </th>
+                    <th>
+                        Year
+                    </th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                {books.map((book,index) => (
+                    <tr key={index}>
+                        <td>{book.title}</td>
+                        <td>{book.description}</td>
+                        <td>{book.author}</td>
+                        <td>{book.year}</td>
+                        <td>
+                            <Link to={`/Update/${book.id}`} className="btn btn-primary">Update</Link>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+        </main>
+    );
+}
